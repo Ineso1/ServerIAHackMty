b03e63ae9fe779b762ac82f8d309a8d38a520683
Ines Alejandro Garcia Mosqueda <inesgamos@gmail.com>
2022-09-25 02:10:08 -0500

    .

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import MainPage from "./src/MainPage.js"
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '<h1>Cola de dani</h1>'
+    return '<MainPage/>'
 
 if __name__ == "__main__":
     app.run(debug=True)
